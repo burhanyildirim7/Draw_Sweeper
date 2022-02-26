cb6e16c7dfdb15ae99204db3769111bced39b2fe
Said Burhan <saidburhan@gmail.com>
2022-02-26 11:36:00 +0300

    coin efekti düzenlendi

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -14,16 +14,27 @@ public class Collector : MonoBehaviour
 			DrawMeshSbi.instance.drawableRelase = true;
 			other.transform.tag = "emptytag";
 			other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-			GameObject coin = Instantiate(goldCoin, other.transform.position + new Vector3(0,0,2), Quaternion.identity);
-			float time = Random.Range(.9f, 1.6f);
-			coin.transform.DOMove(GameController.instance.goldTarget.position, time);
-			coin.transform.DOScale(Vector3.one*.1f, 1.6f).OnComplete(() => 
-			{ 
-				GameController.instance.SetScore(10);
-				Destroy(coin);
-			}); 
+			StartCoroutine(CoinAnimation(other.transform.position));
 			other.tag = "emptytag";
 			GameController.instance.collectingCount++;
 		}
+	}
+
+
+	IEnumerator CoinAnimation(Vector3 position)
+	{
+		yield return new WaitForSeconds(1.5f);
+		GameObject coin = Instantiate(goldCoin, position + new Vector3(0, 0, 1), Quaternion.identity);
+		float time = Random.Range(.9f, 1.6f);
+		//coin.transform.DOJump(position,5,1,2);
+		//coin.transform.DORotate(new Vector3(179,0,0),2);
+		//coin.transform.DOScale(Vector3.one * 2f, 1f).
+		//	OnComplete(() => { coin.transform.DOScale(Vector3.one * .2f,1f); });
+		coin.transform.DOMove(GameController.instance.goldTarget.position, time);
+		coin.transform.DOScale(Vector3.one * .01f, 1.6f).OnComplete(() =>
+		{
+			GameController.instance.SetScore(10);
+			Destroy(coin);
+		});
 	}
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -129,13 +129,14 @@ public class UIController : MonoBehaviour
     /// </summary>
     public void ActivateWinScreen()
     {
-        GamePanel.SetActive(false);
+
         StartCoroutine(WinScreenDelay());
     }
 
     IEnumerator WinScreenDelay()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2.5f);
+        GamePanel.SetActive(false);
         WinPanel.SetActive(true);
         winScreenScoreText.text = "0";
         int sayac = 0;
